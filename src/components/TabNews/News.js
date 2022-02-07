6cc07ba40e86dcede2dd10788d9a1111e685e106
liewkm <94386756+liewkm@users.noreply.github.com>
2022-02-07 23:55:50 +0800

    Change query/q to JS dynamic instead of hardcoded.

--- src/components/TabNews/News.js
+++ src/components/TabNews/News.js
@@ -1,18 +1,22 @@
 import React, { useState, useEffect } from 'react';
 import Axios from 'axios';
 import NewsCard from './NewsCard';
+import { API_NewsApi, API_Json } from '../../api/API';
+
+// const { REACT_APP_API_KEY } = process.env.local.REACT_APP_API_KEY_NEWSAPI;
 
 const News = () => {
     const [articles, setArticles] = useState([]);   // state sets the data in an Array.
+    const [query, setQuery] = useState('tsla');
 
     useEffect(() => {
         const getArticles = async () => {
-            const res = await Axios.get('https://newsapi.org/v2/everything?q=aapl&apiKey=c1c56bdf71c14d1cabebd9a4f6610a9d');
+            const res = await Axios.get(`https://newsapi.org/v2/everything?q=${query}&apiKey=c1c56bdf71c14d1cabebd9a4f6610a9d`);
             console.log(res);
             setArticles(res.data.articles);
         }
         getArticles();
-    }, [])
+    }, [query])
 
     return (
         <div>
