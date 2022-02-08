bd00b145e1988c2f4f56d78a488931bebffcdfe3
LeslieChiang <wslchiang@gmail.com>
2022-02-08 20:42:26 +0800

    update

--- src/components/TabCrypto/ViewConversion.js
+++ src/components/TabCrypto/ViewConversion.js
@@ -82,10 +82,10 @@ export default function ViewConversion() {
       console.log("Chart Data: ", status);
 
       const timeData = data["Time Series (Digital Currency Daily)"];
-      console.log("timeData: ", timeData);
-      console.log("Object.keys: ", Object.keys(timeData)); // dates string
-      console.log("Object.values: ", Object.values(timeData));
-      console.log("Object.entries: ", Object.entries(timeData));
+      // console.log("timeData: ", timeData);
+      // console.log("Object.keys: ", Object.keys(timeData)); // dates string
+      // console.log("Object.values: ", Object.values(timeData));
+      // console.log("Object.entries: ", Object.entries(timeData));
 
       // How to Filter Dates Objects and Display in Chart JS, ref: https://youtu.be/AEaXyzCElGI
       // Object.entries(): [[keys,objects]] - obj>array
@@ -228,7 +228,7 @@ export default function ViewConversion() {
             rate={displayExchange.rate}
           />
         </div>
-        {/* <ViewChart data={cryptoChartData} /> */}
+        <ViewChart data={cryptoChartData} />
       </div>
     </div>
   );
