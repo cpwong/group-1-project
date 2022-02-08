ce6ecda004c96bfe783bf433411e988af4f70e5a
cpwong <wongchenpang@gmail.com>
2022-02-08 17:26:23 +0800

    Merged TabMovies

--- src/components/TabMovies/Movie.js
+++ src/components/TabMovies/Movie.js
@@ -1,29 +1,35 @@
 import React from 'react';
 
-const Movie = ({movie, selectMovie}) => {
-    const IMAGE_PATH = "https://image.tmdb.org/t/p/w342"
+const Movie = ({ movie, selectMovie }) => {
+  const IMAGE_PATH = 'https://image.tmdb.org/t/p/w342';
 
-    return (
-        <div onClick={() => selectMovie(movie)} className={"movie"}>
-            <div className="movie-title">
-                
-                {movie.poster_path &&
-                <div className='pic'>
-                <img src={IMAGE_PATH + movie.poster_path} alt={movie.title} className="imageq"/>
-                <div className='overview'>
-                <div><h5> {movie.overview}</h5></div>
-                </div>
-                </div>
-                }
-                
-                <div className={"flex between movie-infos"}>
-                    <h5 className={"movie-title"}>{movie.title}</h5>
-                    {movie.vote_average ? <span className={"movie-voting"}>{movie.vote_average}</span> : null}
-                </div>
+  return (
+    <div onClick={() => selectMovie(movie)} className={'movie'}>
+      <div className='mani-movie-title'>
+        {movie.poster_path && (
+          <div className='mani-pic'>
+            <img
+              src={IMAGE_PATH + movie.poster_path}
+              alt={movie.title}
+              className='mani-imageq'
+            />
+            <div className='mani-overview'>
+              <div>
+                <h5> {movie.overview}</h5>
+              </div>
             </div>
-        </div>
-    );
-};
+          </div>
+        )}
 
+        <div className={'mani-flex mani-between mani-movie-infos'}>
+          <h5 className={'mani-movie-title'}>{movie.title}</h5>
+          {movie.vote_average ? (
+            <span className={'mani-movie-voting'}>{movie.vote_average}</span>
+          ) : null}
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Movie;
